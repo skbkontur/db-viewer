ec4272d17945762b5061279d793e894348eb534f
Den Mukhametianov <den.mukhametianov@gmail.com>
2019-03-25 10:14:10 +0500

    tc tasks

--- cake/Build.cs
+++ cake/Build.cs
@@ -14,8 +14,18 @@ namespace cake
             var slnName = "DBViewer.sln";
             var srcDirectory = Path.Combine(moduleDirectory, @"src");
             var mainSuite = Path.Combine(moduleDirectory, @"serviceSuites\startAll.yaml");
+            var cassandraSuite = Path.Combine(cementDirectory, @"alco.global-services\serviceSuites\cassandra3.yaml");
             var pathToTestsDll = "src/Tests/bin/Kontur.DBViewer.Tests.dll";
-            var suites = new[] {mainSuite};
+            var suites = new[] {mainSuite, cassandraSuite};
+            
+            var startServices = CakeTask("StartServices").Does(() =>
+                {
+                    Cake.StartServices(cementDirectory, suites, true);
+                });
+            var stopServices = CakeTask("StopServices").Does(() =>
+                {
+                    Cake.StopServices(cementDirectory, suites);
+                });
 
             var restorePackages =
                 CakeTask("RestorePackages")
@@ -25,11 +35,14 @@ namespace cake
                 CakeTask("ServiceRunner")
                     .Does(() => { Cake.StartServices(cementDirectory, suites, false); });
 
-            var functionalTests =
+            var tests =
                 CakeTask("Test", false)
                     .Does(() => { Cake.RunTests(cementDirectory, Path.Combine(moduleDirectory, pathToTestsDll)); });
-
-
+            
+            var tcIntegrationTests = CakeTask("TcTest").Does(() => { })
+                                                                   .IsDependentOn(startServices)
+                                                                   .IsDependentOn(tests)
+                                                                   .Finally(() => stopServices.Task.Execute(Cake).Wait());
         }
     }
 }
