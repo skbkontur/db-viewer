29479ac3353866862e562c9b8ce0a7ae47d73cd3
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2020-03-30 21:53:08 +0500

    fix scripts

--- db-viewer-ui/patch-version.js
+++ db-viewer-ui/patch-version.js
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+const fs = require("fs");
 const process = require("process");
 
 const nbgv = require("nerdbank-gitversioning");
 
 process.chdir("./dist");
-nbgv.setPackageVersion();
+nbgv.setPackageVersion().then(() => fs.writeFileSync(".npmrc", "//registry.npmjs.org/:_authToken=${npm_auth_token}"));
