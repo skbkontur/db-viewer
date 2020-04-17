a34a56689ed6e8e3587d92a39c8287dc5d72dbf1
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2020-04-18 01:08:57 +0500

    remove redundant json serialization configuration

--- DbViewer.TestApi/WebApiService.cs
+++ DbViewer.TestApi/WebApiService.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Formatting;
-using System.Reflection;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
-using Cassandra;
-using Cassandra.Mapping.Attributes;
-
 using Microsoft.Owin.Hosting;
-
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Serialization;
 
 using Owin;
 
@@ -29,14 +18,6 @@ namespace SkbKontur.DbViewer.TestApi
                     var cors = new EnableCorsAttribute("*", "*", "*");
                     config.EnableCors(cors);
                     config.MapHttpAttributeRoutes();
-                    config.Formatters.Clear();
-                    config.Formatters.Add(new JsonMediaTypeFormatter
-                        {
-                            SerializerSettings = new JsonSerializerSettings
-                                {
-                                    Converters = new JsonConverter[] {new CqlObjectPropertyToStringConverter()}
-                                },
-                        });
                     config.EnsureInitialized();
                     appBuilder.UseWebApi(config);
                 });
@@ -48,38 +29,5 @@ namespace SkbKontur.DbViewer.TestApi
         }
 
         private IDisposable service;
-    }
-
-    public class CqlObjectPropertyToStringConverter : JsonConverter
-    {
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            var dictionary = new Dictionary<string, object>();
-            foreach (var property in value.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
-            {
-                if (property.PropertyType == typeof(LocalDate) || property.PropertyType == typeof(TimeUuid))
-                {
-                    dictionary[property.Name] = property.GetValue(value).ToString();
-                }
-                else
-                {
-                    dictionary[property.Name] = property.GetValue(value);
-                }
-            }
-
-            staticSerializer.Serialize(writer, dictionary);
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType.GetCustomAttributes<TableAttribute>().Any();
-        }
-
-        private readonly JsonSerializer staticSerializer = new JsonSerializer {ContractResolver = new DefaultContractResolver()};
     }
 }
