cdc58bf6b9e5fb13d1d06ae5b3eebc195b886bf3
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2020-04-18 20:17:03 +0500

    Remove TypeScriptGenerator project

--- .rebuild.cmd
+++ .rebuild.cmd
@@ -1,11 +1,3 @@
 dotnet build --force --no-incremental --configuration Release ./DbViewer.sln
 
-REM dotnet tool restore
-REM dotnet ts-gen ^
-REM     --assembly ./DbViewer.TestApi/bin/Release/net472/SkbKontur.DbViewer.TestApi.exe ^
-REM     --outputDir ./db-viewer-ui/src/Domain/Api ^
-REM     --nullabilityMode NullableReference
-
-"./DbViewer.TypeScriptGenerator/bin/Release/net472/SkbKontur.DbViewer.TypeScriptGenerator.exe" 
-
 pause
