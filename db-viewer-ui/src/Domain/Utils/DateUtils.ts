a24d6a2afa803814b0ca9616075371f08c53071e
olga.karpova <olga.karpova@skbkontur.ru>
2022-08-18 18:51:54 +0300

    Replace moment js with date-fns

--- db-viewer-ui/src/Domain/Utils/DateUtils.ts
+++ db-viewer-ui/src/Domain/Utils/DateUtils.ts
@@ -1,9 +1,9 @@
-import moment from "moment";
+import { addMinutes, format, parse } from "date-fns";
 
 import { RussianDateFormat } from "../DataTypes/DateTimeRange";
 
 export class DateUtils {
-    private static readonly datePickerFormat: RussianDateFormat = "DD.MM.YYYY";
+    private static readonly datePickerFormat: RussianDateFormat = "dd.MM.yyyy";
 
     public static isCorrectTime(time: string): boolean {
         return Boolean(time.match(/^([01]?[0-9]|2[0-3]):[0-5][0-9]/));
@@ -11,13 +11,18 @@ export class DateUtils {
 
     public static convertDateToString(
         date: Date | string,
-        timeZone: number,
-        format: string = this.datePickerFormat
+        timeZone: number | null,
+        dateFormat: string = this.datePickerFormat
     ): string {
-        return moment.utc(date).utcOffset(timeZone).format(format);
+        return format(timeZone == null ? new Date(date) : this.toTimeZone(new Date(date), timeZone), dateFormat);
     }
 
     public static convertStringToDate(date: RussianDateFormat): Date {
-        return moment(date, this.datePickerFormat).toDate();
+        return parse(date, this.datePickerFormat, new Date());
+    }
+
+    private static toTimeZone(date: Date | string, timeZoneOffsetInMinutes: number): Date {
+        const dateDate = new Date(date);
+        return addMinutes(dateDate, dateDate.getTimezoneOffset() + timeZoneOffsetInMinutes);
     }
 }
