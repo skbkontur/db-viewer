a14913107d566ea930b15a5817947ba6985a3237
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2024-01-17 09:15:01 +0500

    update storybook

--- db-viewer-ui/.storybook/main.ts
+++ db-viewer-ui/.storybook/main.ts
@@ -1,5 +1,9 @@
-module.exports = {
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
     stories: ["../stories/**/*.stories.tsx"],
     addons: ["@storybook/addon-actions"],
     framework: "@storybook/react-vite",
 };
+
+export default config;
--- db-viewer-ui/stories/Themes/ObjectError.stories.tsx
+++ db-viewer-ui/stories/Themes/ObjectError.stories.tsx
@@ -1,9 +1,9 @@
 import {
     DEFAULT_THEME,
-    DEFAULT_THEME_8PX_OLD,
-    FLAT_THEME_8PX_OLD,
+    DARK_THEME,
+    THEME_2022,
+    THEME_2022_DARK,
     ThemeContext,
-    ThemeFactory,
 } from "@skbkontur/react-ui";
 import { Theme } from "@skbkontur/react-ui/lib/theming/Theme";
 import React from "react";
@@ -12,8 +12,6 @@ import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { NullCustomRenderer } from "../../src";
 import { ObjectDetailsContainer } from "../../src/Containers/ObjectDetailsContainer";
 import { DbViewerApiFake } from "../Api/DbViewerApiFake";
-
-import { reactUiDark } from "./reactUiDark";
 
 export default {
     title: "Themes/ObjectError",
@@ -40,6 +38,6 @@ const ErrorContainer = ({ theme }: { theme: Theme }) => (
 );
 
 export const Default = (): React.ReactElement => <ErrorContainer theme={DEFAULT_THEME} />;
-export const Flat = (): React.ReactElement => <ErrorContainer theme={FLAT_THEME_8PX_OLD} />;
-export const Old = (): React.ReactElement => <ErrorContainer theme={DEFAULT_THEME_8PX_OLD} />;
-export const Dark = (): React.ReactElement => <ErrorContainer theme={ThemeFactory.create(reactUiDark)} />;
+export const Dark = (): React.ReactElement => <ErrorContainer theme={DARK_THEME} />;
+export const Theme2022 = (): React.ReactElement => <ErrorContainer theme={THEME_2022} />;
+export const Theme2022Dark = (): React.ReactElement => <ErrorContainer theme={THEME_2022_DARK} />;
